59b3e469390cc6736dd5756fcf295d88d615d10e
Yana Kashtanova <roadof7winds@ruhealth.net>
2017-07-13 15:17:17 +0300

    optimize code

--- js/collapse-info.js
+++ js/collapse-info.js
@@ -16,16 +16,48 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
-	$(".collapse-doctor-info").click(function () {
-		$(this).closest('.doctor-info-block').find(".doctor-info-article").slideToggle(300);
-		
-		if ($(this).text()=="развернуть")
+  $(".collapse-doctor-info").click(function () {
+    $(this).closest('.doctor-info-block').find(".doctor-info-article").slideToggle(300);
+    
+    if ($(this).text()=="показать")
             {
                $(this).text("свернуть")
             }
         else
             {
-               $(this).text("развернуть")
+               $(this).text("показать")
+            };
+      return false;
+    });
+});
+
+$(document).ready(function(){
+  $(".collapse-file-info").click(function () {
+    $(this).closest('.attached').find(".record-list").slideToggle(300);
+    
+    if ($(this).text()=="показать")
+            {
+               $(this).text("свернуть")
+            }
+        else
+            {
+               $(this).text("показать")
+            };
+      return false;
+    });
+});
+
+$(document).ready(function(){
+  $(".collapse-consult-month").click(function () {
+    $(this).closest('.consult-list').find(".consult-list-month-item").slideToggle(300);
+    
+    if ($(this).text()=="показать")
+            {
+               $(this).text("свернуть")
+            }
+        else
+            {
+               $(this).text("показать")
             };
       return false;
     });
@@ -55,4 +87,15 @@ $("body").on('click', '[href*="#"]', function(e){
   var fixed_offset = 100;
   $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
   e.preventDefault();
+});
+
+$('.to-favorites').click(function(){
+  $(this).toggleClass('active');
+});
+
+//calendar
+$('.new-calendar a').on('click', function(e) {
+    $('.new-calendar a').removeClass('active-time');
+    $(this).addClass('active-time');
+    e.preventDefault(); 
 });
